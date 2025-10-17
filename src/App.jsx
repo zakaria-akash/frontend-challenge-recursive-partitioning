f07ec98ce79949cfd76a6fdacde7c91464ba92a9
zakaria-akash <zakaria.93@yahoo.com>
2025-10-17 13:01:10 +0600

    clone and install packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <h1 className="text-3xl font-bold underline text-center text-blue-600">
+      Hello world!!!
+    </h1>
+  );
 }
 
 export default App;
